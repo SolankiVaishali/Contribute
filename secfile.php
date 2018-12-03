eb9063bb39aaf227e82784f7fe96722eb8d4f8eb
SRIMCA\201506100610077 <SRIMCA\201506100610077@cs-u611.srimca.local>
2018-12-03 13:14:16 +0530

    this is netbeans 2nd commit in ut2 sec file

--- secfile.php
+++ secfile.php
@@ -1,9 +1,5 @@
 <?php
 
 echo "This is Second file in IDE";
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 ?>
